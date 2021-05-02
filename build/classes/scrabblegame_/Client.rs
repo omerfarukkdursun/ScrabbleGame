32197d6c61d782c7a8a89148af6869cd3b7d7abc
Omer Faruk Dursun <omerfarukkdursun@gmail.com>
2021-05-02 16:49:18 +0300

    Harf sayısı arttırıldı
    Harf sayısı arttırıldı ve tasarımsal sorunlar çözüldü.

--- build/classes/scrabblegame_/Client.rs
+++
@@ -1,2 +0,0 @@
-scrabblegame_.Listen
-scrabblegame_.Client
--- src/scrabblegame_/ScrabbleGame_.java
+++ src/scrabblegame_/ScrabbleGame_.java
@@ -36,13 +36,49 @@ public class ScrabbleGame_ {
 
     public static Image p = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\p.png");
     public static Image u = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\u.png");
+    public static Image a = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\a.png");
+    public static Image k = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\k.png");
+    public static Image l = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\l.png");
+    public static Image o = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\o.png");
+    public static Image b = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\b.png");
+  
+    public static Image m = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\m.png");
+   
+    public static Image e = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\e.png");
+    public static Image t = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\t.png");
+    public static Image s = gorsel("C:\\Users\\Fatih\\Desktop\\ScrableGame\\ScrabbleGame\\src\\scrabblegame_\\gorseller\\s.png");
+
     public static boolean isP;
     public static boolean isU;
+    public static boolean isA;
+    public static boolean isK;
+    public static boolean isL;
+    public static boolean isO;
+    public static boolean isB;
+  
+    public static boolean isM;
+    
+    public static boolean isE;
+    public static boolean isT;
+    public static boolean isS;
+
     public static ArrayList<Image> yazilanHarflerinResimleri = new ArrayList<Image>();
     public static ArrayList<Point> yazilanHarflerinKordinatlari = new ArrayList<Point>();
 
     public static harf uHarfi = new harf("U", u);
     public static harf pHarfi = new harf("P", p);
+
+    public static harf aHarfi = new harf("A", a);
+    public static harf kHarfi = new harf("K", k);
+    public static harf lHarfi = new harf("L", l);
+    public static harf oHarfi = new harf("O", o);
+    public static harf bHarfi = new harf("B", b);
+
+    public static harf mHarfi = new harf("M", m);
+   
+    public static harf eHarfi = new harf("E", e);
+    public static harf tHarfi = new harf("T", t);
+    public static harf sHarfi = new harf("S", s);
 
     public static ArrayList<harf> harflerListesi = new ArrayList<harf>();
     public static ArrayList yazilanHarfler = new ArrayList();
@@ -59,9 +95,9 @@ public class ScrabbleGame_ {
     public static JFrame jFrame = new JFrame();
 
     public static String RandomLetter() {
-        String[] strArr = {"P", "Q", "R", "S", "T", "U", "V", "W"};
+        String[] strArr = { "B",  "M", "E", "T", "S","P", "O", "A", "K", "L", "U"};
         Random rand = new Random();
-        return strArr[rand.nextInt(6)];
+        return strArr[rand.nextInt(11)];
 
     }
 
@@ -110,6 +146,35 @@ public class ScrabbleGame_ {
         if ((String) msgContent.get(1) == "U") {
             yazilanHarflerinResimleri.add(u);
         }
+        if ((String) msgContent.get(1) == "A") {
+            yazilanHarflerinResimleri.add(a);
+        }
+        if ((String) msgContent.get(1) == "K") {
+            yazilanHarflerinResimleri.add(k);
+        }
+        if ((String) msgContent.get(1) == "L") {
+            yazilanHarflerinResimleri.add(l);
+        }
+        if ((String) msgContent.get(1) == "O") {
+            yazilanHarflerinResimleri.add(o);
+        }
+        if ((String) msgContent.get(1) == "B") {
+            yazilanHarflerinResimleri.add(b);
+        }
+      
+        if ((String) msgContent.get(1) == "M") {
+            yazilanHarflerinResimleri.add(m);
+        }
+       
+        if ((String) msgContent.get(1) == "E") {
+            yazilanHarflerinResimleri.add(e);
+        }
+        if ((String) msgContent.get(1) == "T") {
+            yazilanHarflerinResimleri.add(t);
+        }
+        if ((String) msgContent.get(1) == "S") {
+            yazilanHarflerinResimleri.add(s);
+        }
 
     }
 
@@ -118,12 +183,58 @@ public class ScrabbleGame_ {
         if (s == "P") {
             isP = true;
             puan += 4;
-            System.out.println(puan);
+
         }
         if (s == "U") {
             isU = true;
             puan += 2;
-            System.out.println(puan);
+
+        }
+        if (s == "A") {
+            isA = true;
+            puan += 1;
+
+        }
+        if (s == "K") {
+            isK = true;
+            puan += 7;
+
+        }
+        if (s == "L") {
+            isL = true;
+            puan += 3;
+
+        }
+        if (s == "O") {
+            isO = true;
+            puan += 3;
+
+        }
+        if (s == "B") {
+            isB = true;
+            puan += 5;
+
+        }
+       
+        if (s == "M") {
+            isM = true;
+            puan += 2;
+
+        }
+       
+        if (s == "E") {
+            isE = true;
+            puan += 4;
+
+        }
+        if (s == "T") {
+            isT = true;
+            puan += 5;
+
+        }
+        if (s == "S") {
+            isS = true;
+            puan += 6;
 
         }
 
@@ -133,8 +244,18 @@ public class ScrabbleGame_ {
 
         harflerListesi.add(uHarfi);
         harflerListesi.add(pHarfi);
+        harflerListesi.add(aHarfi);
+        harflerListesi.add(kHarfi);
+        harflerListesi.add(lHarfi);
+        harflerListesi.add(oHarfi);
+        harflerListesi.add(bHarfi);
+        harflerListesi.add(mHarfi);
+        harflerListesi.add(tHarfi);
+        harflerListesi.add(eHarfi);
+        harflerListesi.add(sHarfi);
+        
 
-        jFrame.setSize(618, 1000);
+        jFrame.setSize(618, 870);
 
         Client.Start("127.0.0.1", 2000);
 
@@ -142,7 +263,7 @@ public class ScrabbleGame_ {
         LinkedList<Kare> kutular = new LinkedList<>();
 
         JButton b1 = new JButton(RandomLetter());
-        b1.setBounds(25, 610, 45, 45);
+        b1.setBounds(25, 610, 55, 45);
         b1.setBackground(Color.yellow);
         jFrame.add(b1);
 
@@ -155,7 +276,7 @@ public class ScrabbleGame_ {
         });
 
         JButton b2 = new JButton(RandomLetter());
-        b2.setBounds(110, 610, 45, 45);
+        b2.setBounds(110, 610, 55, 45);
         b2.setBackground(Color.yellow);
         jFrame.add(b2);
 
@@ -167,7 +288,7 @@ public class ScrabbleGame_ {
         });
 
         JButton b3 = new JButton(RandomLetter());
-        b3.setBounds(195, 610, 45, 45);
+        b3.setBounds(195, 610, 55, 45);
         b3.setBackground(Color.yellow);
         jFrame.add(b3);
 
@@ -179,7 +300,7 @@ public class ScrabbleGame_ {
         });
 
         JButton b4 = new JButton(RandomLetter());
-        b4.setBounds(280, 610, 45, 45);
+        b4.setBounds(280, 610, 55, 45);
         b4.setBackground(Color.yellow);
         jFrame.add(b4);
 
@@ -191,7 +312,7 @@ public class ScrabbleGame_ {
         });
 
         JButton b5 = new JButton(RandomLetter());
-        b5.setBounds(365, 610, 45, 45);
+        b5.setBounds(365, 610, 55, 45);
         b5.setBackground(Color.yellow);
         jFrame.add(b5);
 
@@ -203,7 +324,7 @@ public class ScrabbleGame_ {
         });
 
         JButton b6 = new JButton(RandomLetter());
-        b6.setBounds(450, 610, 45, 45);
+        b6.setBounds(450, 610, 55, 45);
         b6.setBackground(Color.yellow);
         jFrame.add(b6);
         b6.addActionListener(new ActionListener() {
@@ -213,7 +334,7 @@ public class ScrabbleGame_ {
             }
         });
         JButton b7 = new JButton(RandomLetter());
-        b7.setBounds(535, 610, 45, 45);
+        b7.setBounds(535, 610, 55, 45);
         b7.setBackground(Color.yellow);
         jFrame.add(b7);
 
@@ -225,7 +346,7 @@ public class ScrabbleGame_ {
             }
         });
         JButton Gonder = new JButton("Hamle Yap");
-        Gonder.setBounds(250, 680, 100, 50);
+        Gonder.setBounds(225, 680, 100, 50);
         Gonder.setBackground(Color.ORANGE);
         jFrame.add(Gonder);
 
@@ -264,14 +385,11 @@ public class ScrabbleGame_ {
                     Client.Send(msg2);
 
                 }
-                
-                
+
                 Bitir.setEnabled(false);
                 Gonder.setEnabled(false);
-                
 
             }
-            
 
         });
 
@@ -374,6 +492,162 @@ public class ScrabbleGame_ {
                         Logger.getLogger(ScrabbleGame_.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }
+                if (isA) {
+                    try {
+
+                        Point point = sigdir(mouseClickX, mouseClickY);
+                        yazilanHarflerinKordinatlari.add(point);
+                        yazilanHarflerinResimleri.add(a);
+                        Thread.sleep(10);
+                        data.add(point);
+                        data.add("A");
+                        System.out.println("a ya tıklandı");
+                        yazilanHarfler.add(data);
+
+                        isA = false;
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ScrabbleGame_.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+                if (isK) {
+                    try {
+
+                        Point point = sigdir(mouseClickX, mouseClickY);
+                        yazilanHarflerinKordinatlari.add(point);
+                        yazilanHarflerinResimleri.add(k);
+                        Thread.sleep(10);
+                        data.add(point);
+                        data.add("K");
+                        System.out.println("k ya tıklandı");
+                        yazilanHarfler.add(data);
+
+                        isK = false;
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ScrabbleGame_.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                if (isL) {
+                    try {
+
+                        Point point = sigdir(mouseClickX, mouseClickY);
+                        yazilanHarflerinKordinatlari.add(point);
+                        yazilanHarflerinResimleri.add(l);
+                        Thread.sleep(10);
+                        data.add(point);
+                        data.add("L");
+                        System.out.println("l ye tıklandı");
+                        yazilanHarfler.add(data);
+
+                        isL = false;
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ScrabbleGame_.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                if (isO) {
+                    try {
+
+                        Point point = sigdir(mouseClickX, mouseClickY);
+                        yazilanHarflerinKordinatlari.add(point);
+                        yazilanHarflerinResimleri.add(o);
+                        Thread.sleep(10);
+                        data.add(point);
+                        data.add("O");
+                        System.out.println("o ya tıklandı");
+                        yazilanHarfler.add(data);
+
+                        isO = false;
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ScrabbleGame_.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                if (isB) {
+                    try {
+
+                        Point point = sigdir(mouseClickX, mouseClickY);
+                        yazilanHarflerinKordinatlari.add(point);
+                        yazilanHarflerinResimleri.add(b);
+                        Thread.sleep(10);
+                        data.add(point);
+                        data.add("B");
+                        System.out.println("b ye tıklandı");
+                        yazilanHarfler.add(data);
+
+                        isB = false;
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ScrabbleGame_.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+              
+                if (isM) {
+                    try {
+
+                        Point point = sigdir(mouseClickX, mouseClickY);
+                        yazilanHarflerinKordinatlari.add(point);
+                        yazilanHarflerinResimleri.add(m);
+                        Thread.sleep(10);
+                        data.add(point);
+                        data.add("M");
+                       
+                        yazilanHarfler.add(data);
+
+                        isM = false;
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ScrabbleGame_.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+              
+                if (isE) {
+                    try {
+
+                        Point point = sigdir(mouseClickX, mouseClickY);
+                        yazilanHarflerinKordinatlari.add(point);
+                        yazilanHarflerinResimleri.add(e);
+                        Thread.sleep(10);
+                        data.add(point);
+                        data.add("E");
+                        
+                        yazilanHarfler.add(data);
+
+                        isE = false;
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ScrabbleGame_.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                if (isT) {
+                    try {
+
+                        Point point = sigdir(mouseClickX, mouseClickY);
+                        yazilanHarflerinKordinatlari.add(point);
+                        yazilanHarflerinResimleri.add(t);
+                        Thread.sleep(10);
+                        data.add(point);
+                        data.add("T");
+                        
+                        yazilanHarfler.add(data);
+
+                        isT = false;
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ScrabbleGame_.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                if (isS) {
+                    try {
+
+                        Point point = sigdir(mouseClickX, mouseClickY);
+                        yazilanHarflerinKordinatlari.add(point);
+                        yazilanHarflerinResimleri.add(s);
+                        Thread.sleep(10);
+                        data.add(point);
+                        data.add("S");
+                        
+                        yazilanHarfler.add(data);
+
+                        isS = false;
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ScrabbleGame_.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
 
                 for (int i = 0; i < yazilanHarfler.size(); i++) {
 
@@ -459,15 +733,13 @@ public class ScrabbleGame_ {
 
     public static void puanHesapla() {
         String metin = "";
-        String Puanlar = puan + "/"+rakipPuan;
+        String Puanlar = "Puanınız: " + puan + " --- " + " Rakip Puan: " + rakipPuan;
 
         if (puan > rakipPuan) {
             metin = "Kazandınız";
-            
-            
 
         } else if (puan == rakipPuan) {
-            metin = "BERABERE";
+            metin = "Berabere";
 
         } else {
             metin = "Kaybettiniz";
@@ -475,19 +747,15 @@ public class ScrabbleGame_ {
         }
 
         JButton Sonuc = new JButton(Puanlar);
-        Sonuc.setBounds(25, 800, 50, 50);
+        Sonuc.setBounds(25, 750, 565, 50);
         Sonuc.setBackground(Color.ORANGE);
         jFrame.add(Sonuc);
-        
-        
-        
+
         JButton PuanGoster = new JButton(metin);
-        PuanGoster.setBounds(225, 800, 200, 50);
+        PuanGoster.setBounds(420, 680, 170, 50);
         PuanGoster.setBackground(Color.ORANGE);
         jFrame.add(PuanGoster);
-        
-        
-        
+
         jFrame.repaint();
 
     }
